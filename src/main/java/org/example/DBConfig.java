9b2c20b9d06b618597b6db1c1e78bb2483ab85be
azamatkhu <azamat.khu@educa.jcyl.es>
2026-04-21 13:17:24 +0200

    ddd

--- src/main/java/org/example/DBConfig.java
+++ src/main/java/org/example/DBConfig.java
@@ -1,4 +1,31 @@
 package org.example;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class DBConfig {
+    private static final Properties properties = new Properties();
+    static {
+        try (InputStream input = DBConfig.class
+                .getClassLoader()
+                .getResourceAsStream("db.properties")) {
+            if (input == null) {
+                throw new RuntimeException("No se encontró el archivo db.properties");
+            }
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Error al cargar db.properties", e);
+        }
+    }
+
+    public static String getUrl() {
+        return properties.getProperty("db.url");
+    }
+    public static String getUser() {
+        return properties.getProperty("db.user");
+    }
+    public static String getPassword() {
+        return properties.getProperty("db.password");
+    }
 }
--- src/main/java/org/example/MainCrearEmpleado.java
+++ src/main/java/org/example/MainCrearEmpleado.java
@@ -1,17 +1,28 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class MainCrearEmpleado {
+    public static void main(String[] args) {
+        try (Connection connection = DriverManager.getConnection(
+                DBConfig.getUrl(),
+                DBConfig.getUser(),
+                DBConfig.getPassword())) {
+            System.out.println("Conectado!");
+
+            Statement statement = connection.createStatement();
+
+            String sql = "CREATE TABLE empleado (" +
+                    "id NUMBER PRIMARY KEY, " +
+                    "nombre VARCHAR2(100), " +
+                    "salario NUMBER(10, 2))";
+            statement.executeUpdate(sql);
+            System.out.println("TABLA EMPLEADO CREADO");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
